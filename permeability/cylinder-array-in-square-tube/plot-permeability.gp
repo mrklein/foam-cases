fa05c8294ce5ff2251b9737d811e5d2978a913b5
Alexey Matveichev <github@matveichev.com>
2017-12-18 18:19:35 +0100

    Update
    1. Averaging over cell zone instead of whole mesh.
    2. Added averaging of the pressure and fieldValueDelta for calculation
       of pressure drop across porous zone.
    3. Corrected plotting scripts to account for new data.

--- permeability/cylinder-array-in-square-tube/plot-permeability.gp
+++ permeability/cylinder-array-in-square-tube/plot-permeability.gp
@@ -1,18 +1,27 @@
 #!/usr/bin/env gnuplot
 
-d = '< sed "s%(\(.*\))%\1%" postProcessing/vol-averaged-velocity/0/volFieldValue.dat'
+# Since both function objects has the same write interval, we can join them
+# into file with time, dP, Ux, Uy, Uz columns
+d = '< join -j 1 postProcessing/pressure-delta/0/fieldValueDelta.dat postProcessing/vol-averaged-velocity/0/volFieldValue.dat | sed "s%(\(.*\))%\1%"'
 
-rho = 1000
 nu = 0.8926e-6
-mu = rho*nu
-dP = 10
-L = 2e-3
+
+# Length, width, and height of porous zone
+Lx = 1e-3
+Ly = 1e-3
+Lz = 1e-3
+
+# V_zone value is taken from volFieldValue.dat and is calculated by function
+# object, V_zone_real is calculated as Lx*Ly*Lz
+V_zone = 7.25296131e-10
+V_zone_real = Lx*Ly*Lz
+xi = V_zone/V_zone_real
 
 set xlabel 'Time, s'
 set ylabel 'Permeability, d'
 set grid
 
-plot d using 1:($2/dP*mu*L*1e12) with linespoints lw 2 pointtype 7 pointsize 1 notitle
+plot d using 1:(xi*$3/$2*nu*Lx*1e12) with linespoints lw 2 pointtype 7 pointsize 1 notitle
 pause -1
 
 # vi: set ft=gnuplot:
--- permeability/cylinder-array-in-square-tube/plot-permeability.py
+++ permeability/cylinder-array-in-square-tube/plot-permeability.py
@@ -4,23 +4,22 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-# Tube with square profile
-l = 1e-3
-A = l * l
-
-rho = 1000
 nu = 0.8926e-6
-mu = rho * nu
 
-dP = 10
-L = 2e-3
+V = 7.25296131e-10
+Lx = 1e-3
+Ly = 1e-3
+Lz = 1e-3
+
+xi = V/(Lx*Ly*Lz)
 
 data = np.loadtxt('postProcessing/vol-averaged-velocity/0/volFieldValue.dat',
                   converters={1: lambda s: float(s[1:]),
                               3: lambda s: float(s[:-1])})
+dp = np.loadtxt('postProcessing/pressure-delta/0/fieldValueDelta.dat')
 
 plt.figure(figsize=(8, 6))
-plt.plot(data[:, 0], data[:, 1]/dP*mu*L*1e12, '.-', label='Permeability')
+plt.plot(data[:, 0], xi*data[:, 1]/dp[:, 1]*nu*Lx*1e12, '.-', label='Permeability')
 plt.legend(loc='best')
 plt.xlim(xmin=0, xmax=0.2)
 plt.xlabel('Time, s')
@@ -28,6 +27,6 @@ plt.ylim(ymin=0)
 plt.ylabel('Permeability, d')
 plt.grid(ls=':')
 
-plt.savefig('permeability.pdf', bbox_inches='tight', bbox_padding=0.3)
+# plt.savefig('permeability.pdf', bbox_inches='tight', bbox_padding=0.3)
 
-#plt.show()
+plt.show()
